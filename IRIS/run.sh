775a9d88f36959c9956580276fdbd1f31fc0aff9
mhoritaisj <horita@intersystems.com>
2022-02-28 13:30:44 +0900

    Add conditions on x64 or arm64

--- IRIS/run.sh
+++ IRIS/run.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-docker build . -t iris-comm
+ARCH=`uname -m`
+echo $ARCH
+if [ ${ARCH} = 'aarch64' ] || [ ${ARCH} = 'arm64' ]; then
+    IMAGE=containers.intersystems.com/intersystems/iris-community-arm64:2021.2.0.651.0
+else
+    IMAGE=containers.intersystems.com/intersystems/iris-community:2021.2.0.651.0
+fi
+
+docker build --build-arg IMAGE=${IMAGE} . -t iris-comm
 
 docker run --name iris-comm -p 53773:1972 -p 54773:52773 -v $(pwd)/../work:/work -d iris-comm

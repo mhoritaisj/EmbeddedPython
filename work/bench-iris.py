04dbbbef092bb8883669c839628d12a337984797
mhoritaisj <horita@intersystems.com>
2022-03-01 11:17:40 +0900

    Use docker-compose

--- work/bench-iris.py
+++ work/bench-iris.py
@@ -3,11 +3,16 @@ import iris
 import time
 
 df = pd.read_csv('/work/Anime.csv')
-df = df.fillna({'japanese_name':'', 'episodes':'', 'studio':'', 'release_season':'', 'tags':'', 'rating':0.0, 'release_year':0, 'end_year':0, 'description':'', 'content_warning':'', 'related_mange':'', 'related_anime':'', 'voice_actors':'', 'staff':''})
+df = df.fillna({'japanese_name':'', 'episodes':0, 'studio':'', 'release_season':'', 'tags':'', 'rating':0.0, 'release_year':0, 'end_year':0, 'description':'', 'content_warning':'', 'related_mange':'', 'related_anime':'', 'voice_actors':'', 'staff':''})
 
 start = time.time()
 stmt = iris.sql.prepare('insert into anime (rank, name, japanese_name, type, episodes, studio, release_season, tags, rating, release_year, end_year, description, content_warning, related_mange, related_anime, voice_actors, staff) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
 for k, row in df.iterrows():
     stmt.execute(*list(row))
+end = time.time()
+print(end-start)
+
+start = time.time()
+df2 = iris.sql.exec('select release_year from anime').dataframe()
 end = time.time()
 print(end-start)
--- work/bench.py
+++ work/bench.py
@@ -1,11 +1,25 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import psycopg2
 import time
 
 df = pd.read_csv('/work/Anime.csv')
+df = df.fillna({'japanese_name':'', 'episodes':0, 'studio':'', 'release_season':'', 'tags':'', 'rating':0.0, 'release_year':0, 'end_year':0, 'description':'', 'content_warning':'', 'related_mange':'', 'related_anime':'', 'voice_actors':'', 'staff':''})
+
 engine = create_engine('postgresql://postgres:@localhost/testdb')
+conn = psycopg2.connect('dbname=testdb host=localhost user=postgres')
+cur = conn.cursor()
 
 start = time.time()
+#for k, row in df.iterrows():
+#    cur.execute('insert into anime (rank, name, japanese_name, type, episodes, studio, release_season, tags, rating, release_year, end_year, description, content_warning, related_mange, related_anime, voice_actors, staff) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',  list(row))
 df.to_sql('anime', con=engine, if_exists='replace', index=False)
+#conn.commit()
 end = time.time()
 print(end-start)
+
+#start = time.time()
+#df2 = pd.read_sql_query('select release_year from anime', con=engine)
+#end = time.time()
+#print(df2)
+#print(end-start)
